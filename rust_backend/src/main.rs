b8cef720c2d16c9c675ea96dad9726cb006f9e9f
Bening Ranum <8705838+rbennum@users.noreply.github.com>
2024-06-19 23:28:17 +0700

    feat: basically hello world project for backend

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use axum::{
+    routing,
+    Router,
+    Json
+};
+use std::net::SocketAddr;
+
+#[derive(Serialize, Deserialize)]
+struct ApiResponse {
+    message: String
+}
+
+async fn json_response() -> Json<ApiResponse> {
+    let response = ApiResponse {
+        message: "Hello, world!".to_string(),
+    };
+    Json(response)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", routing::get(json_response));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on {}", addr);
+    let listener = tokio::net::TcpListener::bind(addr)
+        .await
+        .unwrap();
+    axum::serve(listener, app)
+        .await
+        .unwrap();
 }
